5763b92001dc210b7365630afe301d60f568d729
abid-hasan-99 <abid.hasan@findit.id>
2025-04-16 02:27:13 +0600

    refactored button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,6 +6,8 @@ import clsx from "clsx";
 
 type ButtonIconPosition = "left" | "right";
 type ButtonShadow = "sm";
+type ButtonFill = "contained";
+type ButtonVariant = "danger";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: ReactNode;
@@ -14,6 +16,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   containerClassName?: string;
   buttonClassName?: string;
   buttonShadow?: ButtonShadow;
+  fill?: ButtonFill;
+  variant?: ButtonVariant;
 }
 
 const getShadow = (shadow?: ButtonShadow): string | undefined => {
@@ -26,6 +30,18 @@ const getShadow = (shadow?: ButtonShadow): string | undefined => {
   return ButtonShadows?.[shadow];
 };
 
+const getBackgroundColor = ({
+  fill,
+  variant,
+}: {
+  fill?: ButtonFill;
+  variant?: ButtonVariant;
+}): string | undefined => {
+  if (!variant) return;
+
+  if (fill === "contained" && variant === "danger") return "bg-danger";
+};
+
 const Button = (props: ButtonProps) => {
   const {
     label,
@@ -35,14 +51,17 @@ const Button = (props: ButtonProps) => {
     buttonClassName,
     onClick,
     buttonShadow,
+    fill = "contained",
+    variant,
   } = props;
 
   return (
     <button
       className={clsx(
-        "flex justify-center items-center gap-x-2.5 p-2 bg-danger-100 cursor-pointer rounded-10 min-w-max",
+        "flex justify-center items-center gap-x-2.5 p-2 cursor-pointer rounded-10 min-w-max",
         containerClassName,
         getShadow(buttonShadow),
+        getBackgroundColor({ fill, variant }),
         {
           "flex-row-reverse": iconPosition === "left",
         }
